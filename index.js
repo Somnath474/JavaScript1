3e006fbc08e62684f850ffec7043ce72052a17ce
Somnath474 <somnathgupta474@gmail.com>
2025-05-01 00:17:53 +0530

    Random Color

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let btn=document.querySelector("button");
-console.dir(btn);
+let  btn=document.querySelector("button");
+btn.addEventListener("click",function(){
+    let h3=document.querySelector("h3");
+    let randomColor=getRandomColor();
+    h3.innerText=randomColor;
 
-btn.onclick=function(){
-    console.log("Button was clicked");
-};
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomColor;
 
-function sayhello(){
-   alert("Hello!"); 
+    console.log("color updated");
+});
+
+function getRandomColor(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+
+    let color=`rgb(${red}, ${green},${blue})`;
+    return color;
 }
-
-btn.onclick=sayhello;
